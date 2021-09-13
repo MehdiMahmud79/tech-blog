53200cafd57b9e639546de4e3058de68473fe635
mehdi Mahmud <mehdi.mahmud@aol.co.uk>
2021-09-13 18:05:27 +0100

    add server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
 const routes = require("./controllers");
-const sequelize = require("./config/connection");
+const dbConnection = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const helpers = require("./utils/helpers");
 
@@ -25,7 +25,7 @@ const sess = {
   // Cookie Options
   maxAge: 24 * 60 * 60 * 1000, // 24 hours
   store: new SequelizeStore({
-    db: sequelize,
+    db: dbConnection,
   }),
 };
 
@@ -37,6 +37,6 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-sequelize.sync({ force: false }).then(() => {
+dbConnection.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
 });
